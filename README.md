a05d65a038387303d146924c8c4e4c9d72dc76ec
eddy goto <eduardogoto8@gmail.com>
2022-11-01 18:59:47 -0700

    color and save added

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Work-Day-Scheduler
+
 homework 5
+
+## Description
+
+The goal of this project was to create a functioning work day scheduler that included an 8 hour planner wherein I could save plans for the day, a time clock, and time blocks that displayed past, present and future times. Working with JQuery was definitely something that I had to get used to. There was a lot of different documentation and I found myself having to refer back to the website multiple times in order to get everything to work correctly. The only major difficulty I had working with this project was creating the time block function, as figuring out a way to assign the time to each individual block wasn't something we'd ever encountered before. However, I came out of this project happy with the results.
+
+## Installation
+
+This project can be loaded on your local browser.
+
+## Usage
+
+The script can be found on my Github profile: https://github.com/eddygoto/Work-Day-Scheduler.
+The project can be seen on my Github page: https://eddygoto.github.io/Work-Day-Scheduler/.
+![Screenshot](./assets/_Users_eddygoto_Code_Work-Day-Scheduler_index.html.png)
+
+## Credits
+
+W3 Schools, JQuery documentation, Bootstrap documentation
+
+## License
+
+None
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,45 @@
+$(document).ready(function() {
 // For running timer:
-var currentDayEl = $('#currentDay');
-
+    var currentDayEl = $('#currentDay');
 // Use moment to find and format the current time and display it on the page
-function displayTime () {
     var currentTime = moment().format('llll');
     currentDayEl.text(currentTime);
-};
+    var currentHour = moment().format('HH');
 
-// Keeps the timer active while the page is loaded
-setInterval(displayTime, 1000);
-
+// For color blocks:
+// Compare current hour to the hour on the schedule
+$(".time-block").each(function() {
+    // This will split the id of each time block and turn that string into a number
+    var timeBlock = parseInt($(this).attr("id").split('hour')[1]);
+    // Setting colors accordingly
+    if (currentHour == timeBlock) {
+        $(this).addClass("present");
+    } else if (currentHour < timeBlock) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else if (currentHour > timeBlock) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+});
 
 // For saving text:
 // On button click, saves description text
-$(document).ready(function() {
-    $('.btn').on('click', function() {
-        var time = $(this).parent().attr('#id');
-        var descriptionText = $(this).siblings('.description').val();
-        localStorage.setItem(time, descriptionText);
+    $('.saveBtn').click (function(event) {
+        event.preventDefault();
+        var hour = $(this).parent().attr("id");
+        var descriptionText = $(this).siblings(".description").val();
+        localStorage.setItem(hour, descriptionText);
     });
-});
 
+// Pulling saved data
+    $("#hour09 .description").val(localStorage.getItem("hour09"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+});
